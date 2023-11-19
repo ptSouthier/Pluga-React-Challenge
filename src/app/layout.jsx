b38246a8883ef1a752bf68cc1f938d8c5d8692ea
ptSouthier <patrick.southier@hotmail.com>
2023-11-19 02:41:41 -0300

    chore: changes layout file extension to match project pattern

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Pluga | Todas as ferramentas para integrar',
+  description: 'Conheça todas as possibilidades de automatizar suas tarefas manuais (e chatas).',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
