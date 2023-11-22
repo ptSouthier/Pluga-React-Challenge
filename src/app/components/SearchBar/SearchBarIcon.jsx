0fbfe154288528cc7750bf4de40d48a5637e70af
ptSouthier <patrick.southier@hotmail.com>
2023-11-22 03:01:07 -0300

    feat: adding "iconProps" optional prop to SearchBarIcon component

--- src/app/components/SearchBar/SearchBarIcon.jsx
+++ src/app/components/SearchBar/SearchBarIcon.jsx
@@ -1,7 +1,7 @@
-export function SearchBarIcon({ icon: Icon }) {
+export function SearchBarIcon({ icon: Icon, iconProps = {} }) {
   return (
     <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
-      {Icon && <Icon />}
+      {Icon && <Icon {...iconProps} />}
     </div>
   )
 }
