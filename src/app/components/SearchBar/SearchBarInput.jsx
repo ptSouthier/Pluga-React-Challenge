c720177e13310f4cc270db6f11fb87525fd4b891
ptSouthier <patrick.southier@hotmail.com>
2023-11-27 20:37:06 -0300

    feat: making use of debouncing function at handleChange and implementing a defaultValue to SearchBarInput component

--- src/app/components/SearchBar/SearchBarInput.jsx
+++ src/app/components/SearchBar/SearchBarInput.jsx
@@ -1,14 +1,17 @@
 'use client';
-export function SearchBarInput({ placeholder, onInputChange }) {
-  const handleChange = (({ target }) => {
+import useDebounce from "@/src/utils/useDebounce";
+
+export function SearchBarInput({ placeholder, onInputChange, searchValue }) {
+  const handleChange = useDebounce(({ target }) => {
     onInputChange(target.value);
-  });
+  }, 500);
 
   return (
     <input
       name="search-bar"
       className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-12 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 focus:outline-sky-300"
       placeholder={placeholder}
+      defaultValue={searchValue}
       onChange={handleChange}
       type="search"
       data-testid="search-bar-input"
