466dc4a8263e916fe0e1a03a25192ce6d76101fc
ptSouthier <patrick.southier@hotmail.com>
2023-12-12 23:41:01 -0300

    feat: importing and using Analytics component from vercel/analytics package, at layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="pt-br" translate="no">
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
